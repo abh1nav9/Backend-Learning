7a0de5e03d344c4b4b8605302da0959174f8298f
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-08-15 20:42:49 +0530

    HTTP SERVER

--- HTTP Server/index.js
+++ HTTP Server/index.js
@@ -2,10 +2,28 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()} : ${res.url} :  New Request Received \n`;
-    fs.appendFile('log.txt', log, (err, data) => {
-        res.end("Hello from Server");
-    });
+  const log = `${Date.now()} : ${res.url} :  New Request Received \n`;
+  if (req.url === "/favicon.ico") return res.end();
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home Page");
+        break;
+
+      case "/about":
+        res.end("My name is Abhinav Gautam");
+        break;
+
+      case "contact":
+        res.end("No Contact");
+        break;
+
+      default:
+        res.end("404 Error");
+    }
+  });
 });
 
-myServer.listen(3000, () => {console.log("Server Started!")});
+myServer.listen(3000, () => {
+  console.log("Server Started!");
+});
