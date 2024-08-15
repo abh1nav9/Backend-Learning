b0a3f671f9f23c5b9885a2642d451caad9142b41
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-08-15 21:04:05 +0530

    URL Understandings

---
+++ HTTP Server/url.js
@@ -0,0 +1,10 @@
+const example = "https://github.com/abh1nav9?tab=repositories";
+
+/*
+
+https -> Hyper Text Transfer Protocol Secure (Extra SSL Security Layer)
+Github.com -> Domain
+/abh1nav9 -> path
+?tab=repositories -> Query Parameters
+
+*/
