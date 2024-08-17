90bb197a6178224c045bdf90319eb39755b071be
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2024-08-17 14:42:14 +0530

    ExpressJS

--- Express/index.js
+++ Express/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Home Page")
+});
+
+app.get("/about", (req, res) => {
+    return res.send(`Hello ${req.query.name}`);
+    // http://localhost:8000/about?name=Abhinav
+});
+
+app.listen(8000, () => console.log("Server Started"));
